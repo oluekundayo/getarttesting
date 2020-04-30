7671b7c8d121290e2d91df903a95557e87f13d11
oluekundayo <ekundayoolumide1@gmail.com>
2020-04-30 04:43:23 +0100

    Update main-es2015.js

--- main-es2015.js
+++ main-es2015.js
@@ -7,15 +7,15 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// function webpackEmptyAsyncContext(req) {
-// 	// Here Promise.resolve().then() is used instead of new Promise() to prevent
-// 	// uncaught exception popping up in devtools
-// 	return Promise.resolve().then(function() {
-// 		var e = new Error("Cannot find module '" + req + "'");
-// 		e.code = 'MODULE_NOT_FOUND';
-// 		throw e;
-// 	});
-// }
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
 webpackEmptyAsyncContext.keys = function() { return []; };
 webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
 module.exports = webpackEmptyAsyncContext;
